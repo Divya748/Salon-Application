2d0fa15cefcb98b9383cbeca5a75811cdd8df62e
Divya <70427484+Divya748@users.noreply.github.com>
2025-02-05 10:30:45 +0530

    updated

--- Salon-Service/src/main/java/com/eager2code/service/SalonService.java
+++ Salon-Service/src/main/java/com/eager2code/service/SalonService.java
@@ -2,6 +2,6 @@ package com.eager2code.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface SalonService {
 }
--- Salon-Service/src/main/java/com/eager2code/service/impl/SalonServiceImpl.java
+++ Salon-Service/src/main/java/com/eager2code/service/impl/SalonServiceImpl.java
@@ -1,4 +1,7 @@
 package com.eager2code.service.impl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SalonServiceImpl {
 }
